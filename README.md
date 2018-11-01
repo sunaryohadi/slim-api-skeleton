4a3670ed47406c84b78e810b2077340d2a504a2a
Hadi <sunaryohadi@gmail.com>
2018-11-01 22:05:00 +0700

    First commit

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 This is a simple Slim Framework API skeleton based off the Slim-Skeleton.
 
+It using ezSQL Library
+
 There's no view layer and logging is to the error_log().
 
---
+++ www/index.php
@@ -0,0 +1,28 @@
+<?php
+if (PHP_SAPI == 'cli-server') {
+    // To help the built-in PHP dev server, check if the request was actually for
+    // something which should probably be served as a static file
+    $url  = parse_url($_SERVER['REQUEST_URI']);
+    $file = __DIR__ . $url['path'];
+    if (is_file($file)) {
+        return false;
+    }
+}
+
+require __DIR__ . '/../private/vendor/autoload.php';
+
+// Instantiate the app
+$settings = require __DIR__ . '/../private/src/settings.php';
+$app = new \Slim\App($settings);
+
+// Set up dependencies
+require __DIR__ . '/../private/src/dependencies.php';
+
+// Register middleware
+require __DIR__ . '/../private/src/middleware.php';
+
+// Register routes
+require __DIR__ . '/../private/src/routes.php';
+
+// Run app
+$app->run();
